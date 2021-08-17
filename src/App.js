e351f4f00b917295cb9a27cf2a439de8d31ad97d
EmissarKir <61598599+EmissarKir@users.noreply.github.com>
2021-08-17 20:45:21 +0300

    add new files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World меня зовут: Ильин Булат</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
